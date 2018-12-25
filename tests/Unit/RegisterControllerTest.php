6787ea11d83193760def396f286d5a7812d78eb4
Abid Hassan Tokee <hassanabidtokee@gmail.com>
2018-12-25 16:24:13 +0600

    Register Controller Unit Test Added

--- tests/Unit/RegisterControllerTest.php
+++ tests/Unit/RegisterControllerTest.php
@@ -8,13 +8,61 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
+     * Register controller create test.
+     * @test
+     * @dataProvider createDataProvider
      * @return void
      */
-    public function testExample()
+    public function create($name, $email, $password, $passwordConfirmation, $expectedStatusHttp, $expectedJson)
     {
-        $this->assertTrue(true);
+        factory(\App\User::class)->create([
+            'email' => 'jane@doe.com'
+        ]);
+
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->json('POST', '/api/register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $passwordConfirmation
+        ]);
+
+        $response
+            ->assertStatus($expectedStatusHttp)
+            ->assertExactJson($expectedJson);
+    }
+
+    public function createDataProvider()
+    {
+        return [
+            [
+                'name' => 'Jane Doe',
+                'email ' => 'jane@doe.com',
+                'password' => 'john1234',
+                'passwordConfirmation' => 'john1234',
+                'expectedStatusHttp' => 422,
+                'expectedJson' => [
+                    'message' => 'The given data was invalid.',
+                    'errors' => [
+                        'email' => ['The email has already been taken.']
+                    ]
+                ]
+            ],
+            [
+                'name' => 'John Doe',
+                'email ' => 'john@doe.com',
+                'password' => 'john1234',
+                'passwordConfirmation' => 'john1234',
+                'expectedStatusHttp' => 201,
+                'expectedJson' => [
+                    'message' => 'Successfully registered'
+                ]
+            ]
+        ];
     }
 }

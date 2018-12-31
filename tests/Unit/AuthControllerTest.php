80b39e9acab01a947bfb79a235034ad1e40dcc57
Abid Hassan Tokee <hassanabidtokee@gmail.com>
2018-12-31 15:58:28 +0600

    Auth controller unit test added

--- tests/Unit/AuthControllerTest.php
+++ tests/Unit/AuthControllerTest.php
@@ -5,16 +5,49 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 
 class AuthControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
+     * Auth controller login test.
+     * @test
+     * @dataProvider loginDataProvider
      * @return void
      */
-    public function testExample()
+    public function login($email, $password, $expectedStatusHttp, $expectedJson)
     {
-        $this->assertTrue(true);
+        factory(\App\User::class)->create([
+            'email' => 'john@doe.com',
+            'password' => Hash::make('john1234')
+        ]);
+
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->json('POST', '/api/auth/login', [
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        $response
+            ->assertStatus($expectedStatusHttp)
+            ->assertJson($expectedJson);
+    }
+
+    public function loginDataProvider()
+    {
+        return [
+            [
+                'email' => 'jane@doe.com',
+                'password' => 'jane1234',
+                'expectedStatusHttp' => 401,
+                'expectedJson' => [
+                    'error' => 'Unauthorized'
+                ]
+            ]
+        ];
     }
 }

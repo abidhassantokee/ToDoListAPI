34943c8c747f42481887ef1776a3889ff051dbc4
Abid Hassan Tokee <hassanabidtokee@gmail.com>
2019-01-01 12:19:42 +0600

    Auth controller test updated

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,33 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Holds the jwt auth token
+     * @var
+     */
+    protected $token;
+
+    /**
+     * Log the user in
+     */
+    protected function signIn()
+    {
+        factory(\App\User::class)->create([
+            'email' => 'test@test.com',
+            'password' => 'test1234'
+        ]);
+
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->json('POST', '/api/auth/login', [
+            'email' => 'test@test.com',
+            'password' => 'test1234'
+        ]);
+
+        $content = json_decode($response->getContent());
+
+        $this->token = $content->access_token;
+    }
 }
--- tests/Unit/AuthControllerTest.php
+++ tests/Unit/AuthControllerTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Hash;
 
 class AuthControllerTest extends TestCase
 {
@@ -21,7 +20,7 @@ class AuthControllerTest extends TestCase
     {
         factory(\App\User::class)->create([
             'email' => 'john@doe.com',
-            'password' => Hash::make('john1234')
+            'password' => 'john1234'
         ]);
 
         $response = $this->withHeaders([
@@ -47,7 +46,80 @@ class AuthControllerTest extends TestCase
                 'expectedJson' => [
                     'error' => 'Unauthorized'
                 ]
+            ],
+            [
+                'email' => 'john@doe.com',
+                'password' => 'john1234',
+                'expectedStatusHttp' => 200,
+                'expectedJson' => [
+                    'token_type' => 'bearer',
+                    'expires_in' => 3600
+                ]
             ]
         ];
+    }
+
+    /**
+     * Auth controller me test.
+     * @test
+     * @return void
+     */
+    public function me()
+    {
+        $this->signIn();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->token
+        ])->json('POST', '/api/auth/me');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'email' => 'test@test.com'
+            ]);
+    }
+
+    /**
+     * Auth controller logout test.
+     * @test
+     * @return void
+     */
+    public function logout()
+    {
+        $this->signIn();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->token
+        ])->json('POST', '/api/auth/logout');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => 'Successfully logged out'
+            ]);
+    }
+
+    /**
+     * Auth controller refresh test.
+     * @test
+     * @return void
+     */
+    public function refresh()
+    {
+        $this->signIn();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->token
+        ])->json('POST', '/api/auth/refresh');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'token_type' => 'bearer',
+                'expires_in' => 3600
+            ]);
     }
 }
